0a4abf58e8383d9d8a67d88f8ae98d3f4fa16743
Jason Lagao <jasonlagao@gmail.com>
2018-01-18 22:59:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const app = "I don't do much."
 
-var array = ["Milo", "Otis", "Garfield"]
+var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
-  array.push(name);
-  return array;
+  kittens.push(name);
+  return kittens;
 }
